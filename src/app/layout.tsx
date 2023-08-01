d3362e40f4218ad020d341e0745699fae529d192
Clesiano Araújo Rodrigues <clesiorodrigues96@gmail.com>
2023-07-31 22:32:07 -0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
-import './globals.css'
+import './../css/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
+import './../css/bootstrap-grid.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
