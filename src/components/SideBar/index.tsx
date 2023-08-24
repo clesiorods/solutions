d4639bc58e0c971428c0db1ac994fd767a018f4c
Clesiano Araújo Rodrigues <clesiorodrigues96@gmail.com>
2023-08-24 00:14:28 -0300

    .

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,13 +1,17 @@
+import Icon from "../Icon";
+import "./style.css";
 
 
 export default function SideBar() {
 
-
     return (
-        <nav className="h-full w-[70] text-white bg-[#020617f7]">
-            <h1 className="text-2xl font-black cursor-default" >SOLUTIONS</h1>
-
-            a
+        <nav>
+            <header>
+                <h1 className="text-2xl font-black cursor-default text-brand" >SOLUTIONS</h1>
+                <div className="icon_case">
+                    <Icon icon="ep:arrow-left-bold" />
+                </div>
+            </header>
         </nav>
     );
 }
