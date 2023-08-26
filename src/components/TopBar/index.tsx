521c87c51652d092985b9302e31526ff78608dc8
Clesiano Araújo Rodrigues <clesiorodrigues96@gmail.com>
2023-08-26 13:58:52 -0300

    .

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -5,6 +5,11 @@ import "./style.css";
 export default function TopBar() {
     return(
         <nav id="top_bar">
+            <ul id="ul_module_sections" >
+                <li>Dashboard</li>
+                <li>Relatórios</li>
+                <li>Configurações</li>
+            </ul>
             <ThemeSwitcher/>
         </nav>
     );
