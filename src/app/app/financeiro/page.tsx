a470bd42ad589efca26e57b19c81b881a86b7acd
Clesiano Araújo Rodrigues <clesiorodrigues96@gmail.com>
2023-08-29 23:38:45 -0300

    .

--- src/app/app/financeiro/page.tsx
+++ src/app/app/financeiro/page.tsx
@@ -10,7 +10,7 @@ export default async function Resume() {
     // await waitFor(2000);    
 
     return (
-        <main className="p-[30px] max-w-full overflow-y-auto">
+        <main className="p-[6px] pr-[17px] mt-[11px] ml-0 w-full overflow-y-auto">
             <div className="row">
                 <div className="col-md-12 col-xl-6">
                     <div className="row">
@@ -35,18 +35,18 @@ export default async function Resume() {
                     </div>
                 </div>
                 <div className="col-md-12 col-xl-6">
-                    <Card className="h-[478px]" >
+                    <Card className="h-[468px]" >
                     </Card>
                 </div>
             </div>
 
             <div className="row">
                 <div className="col-md-12 col-xl-6">
-                    <Card className="h-[478px]" >
+                    <Card className="h-[468px]" >
                     </Card>
                 </div>
                 <div className="col-md-12 col-xl-6">
-                    <Card className="h-[478px]" >
+                    <Card className="h-[468px]" >
                     </Card>
                 </div>
             </div>
