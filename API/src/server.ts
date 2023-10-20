a375bd805a3fff08b842ddd7f585b140e3a4a429
Clesiano Araújo Rodrigues <clesiorodrigues96@gmail.com>
2023-10-19 22:28:19 -0300

    .

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,4 +1,6 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from 'dotenv'
+import 'dotenv/config'
+ // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
 
 import express from 'express';
@@ -7,16 +9,15 @@ import router from './routes';
 import cors from 'cors';
 
 const app = express();
-const PORT = process.env.APLICATION_PORT || 3344;
+const PORT = process.env.PORT || 50644;
 
 
-const allowedOrigins = ['http://localhost:3000', 'https://recad.flystart.com.br'];
-
-const options: cors.CorsOptions = {
-  origin: allowedOrigins
-};
-app.use(cors(options));
-
+// const allowedOrigins = ['http://localhost:3000', 'https://recad.flystart.com.br'];
+// const options: cors.CorsOptions = {
+//   origin: allowedOrigins
+// };
+// app.use(cors(options));
+app.options('*', cors())
 app.use(express.json());
 app.use(router);
 
@@ -25,7 +26,7 @@ app.use(router);
 
 
 app.listen(PORT, () => {
-  console.clear();
+  // console.clear();
   console.log(`==========================================`);
   console.log(`...Servidor rodando na porta ${PORT}...`);
 });
