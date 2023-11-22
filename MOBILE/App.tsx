2f05dd810af97cc5301c49470e9e328ed911b054
Clesiano Araújo Rodrigues <clesiorodrigues96@gmail.com>
2023-11-22 00:10:01 -0300

    .

--- MOBILE/App.tsx
+++ MOBILE/App.tsx
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { AppRouter } from './src/routes/app.router';
+import * as NavigationBar from 'expo-navigation-bar';
 
 export default function App() {
+  NavigationBar.setBackgroundColorAsync("white");
+  NavigationBar.setButtonStyleAsync("dark");
+
   return (
     <View style={{flex:1}}>
       <AppRouter/>
